7292d72f21ee7ab2702fb416e53a6b2c4c75f9ca
Adam <tttgkm@gmail.com>
2024-07-09 01:02:18 -0700

    Form CSS updates

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,21 @@
 import renderHeaderFooter from './headerfooter.js';
-import { renderSlider } from './display.js';
+import { renderListWithTemplate, renderSlider } from './display.js';
+import { getJson } from './dataServices.js';
 // import { getFandom } from './externalApi.js';
 
 renderHeaderFooter();
 renderSlider('slider-home');
+bookingForm();
+
+
+async function bookingForm() {
+  let destinationList = await getJson('destination-list');
+  let list = destinationList.map((location) => location.name);
+  renderListWithTemplate(list, formOptionFn, '#from-list');
+  renderListWithTemplate(list, formOptionFn, '#to-list');
+}
+
+
+function formOptionFn(listItem) {
+  return `<option value="${listItem}">`
+}
